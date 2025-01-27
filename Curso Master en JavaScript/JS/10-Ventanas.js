0ab82d03c5cac7e7e3bf778bddad3a36447841d1
Israel Camarena <Friscaaar@outlook.com>
2025-01-26 21:31:14 -0500

    Terminando primera parte del curso y primer ejercicio

---
+++ Curso Master en JavaScript/JS/10-Ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//ALERTA 
+//alert("esta es mi alerta !!");
+
+//confirmacion
+
+//var mi_resultado = confirm("¿Estas seguro de querer continuar");
+//console.log(mi_resultado);
+
+//INGRESO DATOS
+var edad = prompt("¿Que edad tienes?", 18);
+
+console.log(edad);
