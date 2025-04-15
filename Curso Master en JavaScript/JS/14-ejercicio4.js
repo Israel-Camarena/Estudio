0bb24061ba559c6d0d18ca9afba3529ca87343e3
Israel-Camarena <Friscaaar@outlook.com>
2025-04-15 17:47:44 -0500

    Terminacion con los ejercicios parte 1

--- Curso Master en JavaScript/JS/14-ejercicio4.js
+++ Curso Master en JavaScript/JS/14-ejercicio4.js
@@ -1,1 +1,13 @@
 'use strict'
+
+//Mostrar todos los numeros impares que hay entre dos numeros introducidos por el usuario 
+
+var numero1 = parseInt(prompt("introduce el primer numero", 0));
+var numero2 = parseInt(prompt("introduce el segundo numero", 0));
+
+while (numero1 < numero2){
+    numero1 ++;
+    if(numero1%2 !=0){
+        console.log("El "+ numero1 + "es impar");
+    }
+}
--- Curso Master en JavaScript/JS/15-ejercicio5.js
+++ Curso Master en JavaScript/JS/15-ejercicio5.js
@@ -1,1 +1,11 @@
 'use strict'
+
+//Muestre todos los numeros divisores de un numero introducido en un prompt
+
+var numero1 = parseInt(prompt("introduce un numero", 1));
+
+for (var i=1; i<= numero1; i++){
+    if(numero1%i == 0){
+        console.log("Divisor: "+i);
+    }
+}
