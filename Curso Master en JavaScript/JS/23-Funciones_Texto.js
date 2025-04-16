5237017b6d1abf81b6b7eb70b854415a0a5716a9
Israel-Camarena <jonathan.israel.2000@homail.com>
2025-04-16 00:01:13 -0500

    Continuacion con el curso funciones de texto

---
+++ Curso Master en JavaScript/JS/23-Funciones_Texto.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//transformacion de textos
+
+var numero = 444;
+var texto1 = "Welcome";
+var texto2 = "Bienvenido a la pagina";
+
+
+
+
+
+
+
+
+/*var Dato = numero.toString();
+    Dato = texto1.toUpperCase();
+    Dato = texto2.toLowerCase();
+console.log( Dato);
+
+//calcular longitud
+
+var nombre = "Jonathan";
+    nombre = ["hoa", "Hola"];
+
+console.log(nombre.length);
+
+//Concatenar textos
+
+var textoTotal = texto1+ " " +texto2;
+    textoTotal = texto1.concat(" "+texto2);
+console.log(textoTotal);*/
